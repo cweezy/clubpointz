d22c04f99990a0fc4c75bfcbee2f746627634946
Jessica Graham <jgraham@amplify.com>
2013-11-16 18:10:13 -0500

    Config sails-mongo

--- config/adapters.js
+++ config/adapters.js
@@ -16,12 +16,21 @@ module.exports.adapters = {
 
   // If you leave the adapter config unspecified 
   // in a model definition, 'default' will be used.
-  'default': 'disk',
+  'default': 'mongo',
 
   // Persistent adapter for DEVELOPMENT ONLY
   // (data is preserved when the server shuts down)
   disk: {
     module: 'sails-disk'
+  },
+
+  mongo: {
+    module: 'sails-mongo',
+    host: 'localhost',
+    port: 27017,
+    user: 'clubpointz',
+    password: 'clubpointzpw',
+    database: 'clubpointz'
   },
 
   // MySQL is the world's most popular relational database.
