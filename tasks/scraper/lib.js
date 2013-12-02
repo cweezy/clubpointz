ad077ce5231d9573aaceef2658419642dbf9edd0
Jessica Graham <jjgraham4@gmail.com>
2013-12-01 20:27:03 -0500

    Fix indentation

--- tasks/scraper/lib.js
+++ tasks/scraper/lib.js
@@ -4,74 +4,75 @@ var constants = require('./constants').constants;
 
 
 var lib = {
-/**
- * Parses keys and labels for a list of text headings.
- * Also transfrom and return headingData by adding any new headings.
- */
-getHeadingData : function (headings, headingData) {
-    var headingData = headingData || {};
-    var resultKeys = [];                                                                                                                  
-    _.each(headings, function (heading) {
-        var text = $(heading).html().replace(/\<br \/\>/g, ' ');
-        // TODO clean this up
-        var key = text.replace(/\s/g, '_').replace('&nbsp;', '_').replace('__', '_').replace('.', '').replace(/\//, '').toLowerCase();
-        resultKeys.push(key);
-        if (!headingData[key]) {
-            headingData[key] = {};
-            headingData[key][constants.DATA_KEYS.HEADING.TEXT] = text;
-            headingData[key][constants.DATA_KEYS.DB_ID] = key;
-        }
-    });
 
-    var returnData = {};
-    returnData.headingData = headingData;
-    returnData.resultKeys = resultKeys;
-    return returnData; 
-},
-
-parseResultsPage : function (browser, race, resultKeys, rowSelector, maxResults, resultsPerPage) {
-    if (race.name) {
-        console.log('\nParsing results for ' + race.name);
-    }
-    var results = [];
-    var parsePage = function (startIndex) {
-        console.log('Parsing results ' + startIndex + '-' + parseInt(startIndex + resultsPerPage));
-
-        var pageBody = browser.html();
-        _.each($(pageBody).find(rowSelector), function (row, i) {
-            results[startIndex + i] = {};
-            results[startIndex + i][constants.DATA_KEYS.RACE_ID] = race.id;
-            _.each($(row).find('td'), function (cell, j) {
-                results[startIndex + i][resultKeys[j]] = $(cell).html();
-            });
+    /**
+     * Parses keys and labels for a list of text headings.
+     * Also transfrom and return headingData by adding any new headings.
+     */
+    getHeadingData : function (headings, headingData) {
+        var headingData = headingData || {};
+        var resultKeys = [];                                                                                                                  
+        _.each(headings, function (heading) {
+            var text = $(heading).html().replace(/\<br \/\>/g, ' ');
+            // TODO clean this up
+            var key = text.replace(/\s/g, '_').replace('&nbsp;', '_').replace('__', '_').replace('.', '').replace(/\//, '').toLowerCase();
+            resultKeys.push(key);
+            if (!headingData[key]) {
+                headingData[key] = {};
+                headingData[key][constants.DATA_KEYS.HEADING.TEXT] = text;
+                headingData[key][constants.DATA_KEYS.DB_ID] = key;
+            }
         });
 
-        var nextButton = $(pageBody).find('a:contains("' + constants.NEXT_BTN_TEXT + ' ' + resultsPerPage + '")');
-        if (results.length < maxResults && $(nextButton).length > 0) {
-            var nextUrl = $(nextButton).attr('href');
-            browser.visit(nextUrl, function () {                                                                                          
-                parsePage(startIndex + resultsPerPage);                
-            });
-        } else {
-            console.log('Parsed ' + results.length + ' results');
-            return results;
+        var returnData = {};
+        returnData.headingData = headingData;
+        returnData.resultKeys = resultKeys;
+        return returnData; 
+    },
+
+    parseResultsPage : function (browser, race, resultKeys, rowSelector, maxResults, resultsPerPage) {
+        if (race.name) {
+            console.log('\nParsing results for ' + race.name);
         }
-    };
-    return parsePage(0);
-},
+        var results = [];
+        var parsePage = function (startIndex) {
+            console.log('Parsing results ' + startIndex + '-' + parseInt(startIndex + resultsPerPage));
 
-makeRaceData : function (id, name, year, details, isClubPoints) {
-    raceData = {};
-    raceData[constants.DATA_KEYS.RACE.ID] = id;                                                                         
-    raceData[constants.DATA_KEYS.DB_ID] = id;                                                                           
-    raceData[constants.DATA_KEYS.RACE.NAME] = name;                                                                     
-    raceData[constants.DATA_KEYS.RACE.IS_CLUB_POINTS_MEN] = isClubPoints[0];                                                 
-    raceData[constants.DATA_KEYS.RACE.IS_CLUB_POINTS_WOMEN] = isClubPoints[1];                                               
-    raceData[constants.DATA_KEYS.YEAR] = year;                                                                          
-    raceData[constants.DATA_KEYS.RACE.DETAILS] = details;
+            var pageBody = browser.html();
+            _.each($(pageBody).find(rowSelector), function (row, i) {
+                results[startIndex + i] = {};
+                results[startIndex + i][constants.DATA_KEYS.RACE_ID] = race.id;
+                _.each($(row).find('td'), function (cell, j) {
+                    results[startIndex + i][resultKeys[j]] = $(cell).html();
+                });
+            });
 
-    return raceData;
-}
+            var nextButton = $(pageBody).find('a:contains("' + constants.NEXT_BTN_TEXT + ' ' + resultsPerPage + '")');
+            if (results.length < maxResults && $(nextButton).length > 0) {
+                var nextUrl = $(nextButton).attr('href');
+                browser.visit(nextUrl, function () {                                                                                          
+                    parsePage(startIndex + resultsPerPage);                
+                });
+            } else {
+                console.log('Parsed ' + results.length + ' results');
+                return results;
+            }
+        };
+        return parsePage(0);
+    },
+
+    makeRaceData : function (id, name, year, details, isClubPoints) {
+        raceData = {};
+        raceData[constants.DATA_KEYS.RACE.ID] = id;                                                                         
+        raceData[constants.DATA_KEYS.DB_ID] = id;                                                                           
+        raceData[constants.DATA_KEYS.RACE.NAME] = name;                                                                     
+        raceData[constants.DATA_KEYS.RACE.IS_CLUB_POINTS_MEN] = isClubPoints[0];                                                 
+        raceData[constants.DATA_KEYS.RACE.IS_CLUB_POINTS_WOMEN] = isClubPoints[1];                                               
+        raceData[constants.DATA_KEYS.YEAR] = year;                                                                          
+        raceData[constants.DATA_KEYS.RACE.DETAILS] = details;
+
+        return raceData;
+    }
 };
 
 exports.lib = lib;
