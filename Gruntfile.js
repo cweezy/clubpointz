3510f9d5b1b2ff64928ac7ed80b07b03a985b9f3
Jessica Graham <jjgraham4@gmail.com>
2013-12-03 08:59:57 -0500

    Remove a bunch of whitespace

--- Gruntfile.js
+++ Gruntfile.js
@@ -490,26 +490,26 @@ module.exports = function (grunt) {
     var args = [arg1, arg2].join(',');
     var quietReporter = require('./' + path.join(SCRAPER_FILE_DIR, 'quietReporter')).quietReporter;
 
-    if (args.indexOf('from_file') !== -1) {                                                                                               
-      var file = fs.readFileSync(path.join(SCRAPER_FILE_DIR, 'races.json'));                                                              
-      process.env.RACES = file;                                                                                                        
-    }                                                                                                                                     
-                                                                                                                                          
-    if (args.indexOf('max_results') !== -1) {                                                                                             
-      var maxResults = args.match(/max_results=([0-9]+)(,|$)/)[1];                                                                        
-      process.env.MAX_RESULTS = maxResults;                                                                                            
-    }                                                                                                                                     
-                                                                                                                                          
-    var done = this.async();                                                                                                              
-    var mocha = new Mocha({                                                                                                               
-      reporter: quietReporter,                                                                                                            
-      timeout: 99999999                                                                                                                   
-    });                                                                                                                                   
-    mocha.addFile(path.join(SCRAPER_FILE_DIR, 'scraper.js'));                                                                             
-    mocha.run(function (failures) {                                                                                                       
-      done();                                                                                                                             
-    });                                                                                                                                   
-  });  
+    if (args.indexOf('from_file') !== -1) {
+      var file = fs.readFileSync(path.join(SCRAPER_FILE_DIR, 'races.json'));
+      process.env.RACES = file;
+    }
+
+    if (args.indexOf('max_results') !== -1) {
+      var maxResults = args.match(/max_results=([0-9]+)(,|$)/)[1];
+      process.env.MAX_RESULTS = maxResults;
+    }
+
+    var done = this.async();
+    var mocha = new Mocha({
+      reporter: quietReporter,
+      timeout: 99999999
+    });
+    mocha.addFile(path.join(SCRAPER_FILE_DIR, 'scraper.js'));
+    mocha.run(function (failures) {
+      done();
+    });
+  });
 
   grunt.registerTask('lint', "Lints javascript", function () {
    
