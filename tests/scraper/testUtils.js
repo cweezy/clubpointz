9fd81a8b35d5d7ccd71e0931feef65dedd4fc1db
Jessica Graham <jjgraham4@gmail.com>
2013-12-06 00:28:13 -0500

    Update scraper util tests

--- tests/scraper/testUtils.js
+++ tests/scraper/testUtils.js
@@ -1,19 +1,19 @@
-var lib = require('./../../tasks/scraper/scraperLib').lib;
+var utils = require('./../../tasks/scraper/utils').utils;
 var assert = require('assert');
 var _ = require('underscore');
 
 
-describe('ScraperLib', function () {
+describe('Scraper utility function', function () {
 
     it('checks if values are times', function (done) {
         var validValues = ['4:4', '05:02', '01:04:30', ':4', '5:'];
         var invalidValues = ['abc4', 'a:5', 'a', '4:$', '%$'];
 
         _.each(validValues, function (val) {
-            assert(lib.isTime(val));
+            assert(utils.isTime(val));
         });
         _.each(invalidValues, function (val) {
-            assert.equal(false, lib.isTime(val));
+            assert.equal(false, utils.isTime(val));
         });
         done();
     }),
@@ -22,7 +22,7 @@ describe('ScraperLib', function () {
         var times = ['01:04:30', '1:00', '1:00:00', '24:59:59'];
         var expectedSeconds = [3870, 60, 3600, 89999];
         _.each(times, function (time, i) {
-            assert.equal(expectedSeconds[i], lib.timeToSeconds(time));
+            assert.equal(expectedSeconds[i], utils.timeToSeconds(time));
         });
         done();
     });
