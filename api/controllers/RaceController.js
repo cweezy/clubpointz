1bc36ae9fa9fbd1ae30e9515c7b2d66bdcef218d
Jessica Graham <jjgraham4@gmail.com>
2013-12-06 09:13:36 -0500

    Clean up controllers, route; allow any params to be passed in URL

--- api/controllers/RaceController.js
+++ api/controllers/RaceController.js
@@ -1,30 +1,23 @@
 /**
  * RaceController
- *
- * @module      :: Controller
- * @description	:: A set of functions called `actions`.
- *
- *                 Actions contain code telling Sails how to respond to a certain type of request.
- *                 (i.e. do stuff, then send some JSON, show an HTML page, or redirect to another URL)
- *
- *                 You can configure the blueprint URLs which trigger these actions (`config/controllers.js`)
- *                 and/or override them with custom routes (`config/routes.js`)
- *
- *                 NOTE: The code you write here supports both HTTP and Socket.io automatically.
- *
- * @docs        :: http://sailsjs.org/#!documentation/controllers
  */
+
+var util = require('./util').util;
+
 
 module.exports = {
     
-  
-
+  find : function (req, res) {
+    var params = util.parseParams(req.params);
+    Race.find(params, function (err, result) {
+      res.json(result);
+    });
+  },
 
   /**
    * Overrides for the settings in `config/controllers.js`
    * (specific to RaceController)
    */
   _config: {}
-
   
 };
--- config/routes.js
+++ config/routes.js
@@ -3,11 +3,6 @@ module.exports.routes = {
   '/raceResults?raceId=:raceId' : {
     controller : 'raceResultsController',
     action : 'index'
-  },
-
-  'get /result/raceId/:id' : {
-    controller : 'result',
-    action : 'findRaceId'
   }
 
 }
