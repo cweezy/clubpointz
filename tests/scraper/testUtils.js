1e5562d8aaf86241248d87e5275727a8aa6f4e24
Jessica Graham <jjgraham4@gmail.com>
2013-12-06 00:40:34 -0500

    Add tests for getting header data

--- tests/scraper/testUtils.js
+++ tests/scraper/testUtils.js
@@ -5,6 +5,28 @@ var _ = require('underscore');
 
 describe('Scraper utility function', function () {
 
+    it('gets heading data', function (done) {
+        var headings = ['<span>Test Heading</span>', '<span>Another One</span>', '<span>a 3rd COOL heading</span>'];
+        var expectedKeys = ['test_heading', 'another_one', 'a_3rd_cool_heading'];
+
+        var headingData = {};
+        var result = utils.getHeadingData(headings, headingData);
+        assert.deepEqual(expectedKeys, result.resultKeys);
+
+        headings = ['<span>1 MoRe HeADInG</span>'];
+        result = utils.getHeadingData(headings, headingData);
+
+        var expectedHeadingData = {
+            'test_heading' : { 'text' : 'Test Heading', '_id' : 'test_heading'  },
+            'another_one' : { 'text' : 'Another One', '_id' : 'another_one'  },
+            'a_3rd_cool_heading' : { 'text' : 'a 3rd COOL heading', '_id' : 'a_3rd_cool_heading'  },
+            '1_more_heading' : { 'text' : '1 MoRe HeADInG', '_id' : '1_more_heading'  }
+        };
+
+        assert.deepEqual(expectedHeadingData, result.headingData);
+        done();
+    }),
+
     it('checks if values are times', function (done) {
         var validValues = ['4:4', '05:02', '01:04:30', ':4', '5:'];
         var invalidValues = ['abc4', 'a:5', 'a', '4:$', '%$'];
