04732e861a7cf9e09921c229e0fcf8c0305be51b
Jessica Graham <jjgraham4@gmail.com>
2013-12-07 09:21:49 -0500

    Add test for getEnvVar util function

--- tests/testUtils.js
+++ tests/testUtils.js
@@ -5,6 +5,13 @@ var _ = require('underscore');
 
 describe('Utility function', function () {
 
+  it('gets environment variables', function (done) {
+    process.env.MOCK = 'mock';
+    assert.equal('mock', util.getEnvVar('MOCK'));
+    assert.equal(null, util.getEnvVar('NOTHING'));
+    done();
+  }),
+
   it('gets plural or singular form of word', function (done) {
     var inputLabels = ['cat', 'cup', 'dollar', 'bear'];
     var inputCounts = [25, 1, 0, 4];
