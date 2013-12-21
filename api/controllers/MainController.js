50507b2a1aa13b236ef530b7b1dbee89fd4b97d0
Christopher Wong <christopher.cl.wong@gmail.com>
2013-12-21 11:28:19 -0500

    adding divisions to teams when sending them to client side

--- api/controllers/MainController.js
+++ api/controllers/MainController.js
@@ -1,15 +1,31 @@
+var _ = require('underscore');
+
+_addDivisionsToTeam = function(teams, divisions) {
+  menATeams = _.findWhere(divisions, {id: 'OPEN MEN A'}).teams;
+  womenATeams = _.findWhere(divisions, {id: 'OPEN WOMEN A'}).teams;
+
+  _.each(teams, function (team) {
+    team.menDivision = _.contains(menATeams, team.name) ? 'A' : 'B';
+    team.womenDivision = _.contains(womenATeams, team.name) ? 'A' : 'B';
+  });
+}
+
 module.exports = {
   index: function(req, res) {
     Race.find({
       year: (new Date()).getFullYear().toString()
     }).done(function (err, races) {
       Team.find().done(function (err, teams) {
-        res.view({
-          races: races,
-          teams: teams
-        });
+        Division.find().done(function (err, divisions) {
+          _addDivisionsToTeam(teams, divisions);
+
+          res.view({
+            races: races,
+            teams: teams
+          });
+        })
       })
     });
-  }
+  },
 
 };
