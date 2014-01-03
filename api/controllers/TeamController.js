7e352ac239d9fdd755cbf0e5263593fd3be6e20a
Christopher Wong <christopher.cl.wong@gmail.com>
2014-01-03 13:23:28 -0500

    team controller gives detailed team results in json api

--- api/controllers/TeamController.js
+++ api/controllers/TeamController.js
@@ -2,12 +2,43 @@
  * TeamController
  */
 
+var _ = require('underscore');
+var Q = require('q');
+
 module.exports = {
 
   /**
    * Overrides for the settings in `config/controllers.js`
    * (specific to TeamController)
    */
-  _config: {}
-  
+  _config: {},
+
+  find: function (req, res) {
+    var teamId = req.param('id');
+    if (!_.isUndefined(teamId)) {
+      Team.findOne(teamId, function foundTeam(err, team) {
+        TeamResult.find({teamId: teamId, isFullTeam: true}, function foundTeamResults(err2, trs) {
+          team.teamResults = trs;
+
+          var promises = [];
+          _.each(trs, function (tr) {
+            promises.push(Result.find({id: tr.resultIds}));
+          });
+
+          Q.allSettled(promises).then(function (results) {
+            _.each(trs, function (tr, idx) {
+              tr.results = results[idx].value;
+            });
+
+            res.json(team);
+          });
+
+        });
+      });
+    } else {
+      Team.find().done(function (err, teams) {
+        res.json(teams);
+      })
+    }
+  }
 };
